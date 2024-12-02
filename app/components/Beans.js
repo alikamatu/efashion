48eac45f47a691522369939e9a10688a9303e571
alikamatu <alikamaatu14@gmail.com>
2024-12-02 20:40:19 +0000

    Update Beans.js

--- app/components/Beans.js
+++ app/components/Beans.js
@@ -18,7 +18,7 @@ export default function Beans() {
     return(
         <div className="md:flex p-2 items-center justify-center gap-4 md:p-10">
             {Bean.map((item, i) => (
-                <div className="md:flex m-3 md:m-0 relative">
+                <div key={i} className="md:flex m-3 md:m-0 relative">
                     <img src={item.image} className="relative" alt="" />
                     <p className="absolute bottom-6 left-4 text-white text-xl">{item.title}</p>
                 </div>
